6365959796df9d42277b7383c00a3c758dc40008
Tushar jain <jaintushar137@gmail.com>
2026-08-24 21:55:29 +0530

    Time: 1492 ms (55.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,6 @@
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans FROM visits v
+LEFT JOIN transactions t
+ON v.visit_id = t.visit_id
+WHERE transaction_id is NULL
+GROUP BY customer_id
+-- Tushar
